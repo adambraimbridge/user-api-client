557f7d892911c3f55bf277da9a614d10a23f01d3
Anurag Kapur <anurag@beancrunch.com>
2016-04-26 22:01:03 +0100

    Refactor happy path test and add invalid email test

--- user-api-client-acceptance-tests/src/test/java/com/ft/membership/userapi/UserApiAsyncClientTest.java
+++ user-api-client-acceptance-tests/src/test/java/com/ft/membership/userapi/UserApiAsyncClientTest.java
@@ -2,12 +2,15 @@ package com.ft.membership.userapi;
 
 import com.ft.aim.client.ErrorResponseException;
 import com.ft.membership.common.types.email.Email;
+import com.ft.membership.userapi.domain.UserProfile;
 import com.ft.membership.userapi.domain.UserProfileCollection;
 import com.ft.membership.userapi.testsupport.TestConfig;
 import me.atam.atam4j.configuration.ConfigLoader;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -15,39 +18,53 @@ import static org.junit.Assert.assertNotNull;
 
 public class UserApiAsyncClientTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserApiAsyncClientTest.class);
     private static TestConfig config;
+
+    UserApiAsyncClient client;
 
     public UserApiAsyncClientTest() {
         config = new ConfigLoader<>(TestConfig.class, "p.yml").getTestConfig();
+        client = new UserApiAsyncClient(config.getApiKey());
     }
 
     @Test
-    public void testGetUserProfileByValidEmailReturnsStatus200AndUserId() throws Exception {
+    public void testGetUserProfileByValidEmailValidUserId() throws Exception {
 
-        UserApiAsyncClient client = new UserApiAsyncClient(config.getApiKey());
+        UserProfileCollection items = null;
 
-        UserProfileCollection items = client
-                .getUserProfileByEmail(new Email(config.getTestRegisteredUser1Email()), Optional.empty())
-                .exceptionally(throwable -> {
-
-                    if(throwable.getCause().getClass().equals(ErrorResponseException.class)) {
-                        ErrorResponseException errorResponseException = (ErrorResponseException) throwable.getCause();
-                        try {
-                            errorResponseException.getResponse().getResponseBody();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        throwable.printStackTrace();
-                    }
-                    return null;
-                })
-                .get();
+        try {
+             items = client
+                     .getUserProfileByEmail(new Email(config.getTestRegisteredUser1Email()), Optional.empty())
+                    .get();
+        } catch(Exception e) {
+            if(e.getCause().getClass().equals(ErrorResponseException.class)) {
+                ErrorResponseException errorResponseException = (ErrorResponseException) e.getCause();
+                LOGGER.error(
+                        "Error response code {} returned by user-api",
+                        errorResponseException.getResponse().getStatusCode());
+                LOGGER.error(errorResponseException.getResponse().getResponseBody());
+            } else {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
 
         assertNotNull(items);
         assertEquals(
                 "UserId returned by the api doesn't match expected value",
                 config.getTestRegisteredUser1UserId(),
                 items.getUserProfiles().get(0).getUserId().toString());
+    }
+
+    @Test
+    public void testGetUserProfileByInvalidEmailShouldReturnEmptyListInResponse() throws Exception {
+
+        Email invalidEmail = new Email("invalidemail1@example.com");
+        List<UserProfile> userProfiles = client
+                .getUserProfileByEmail(invalidEmail, Optional.empty())
+                .get()
+                .getUserProfiles();
+
+        assertEquals("Number of user profiles returned should be 0", 0, userProfiles.size());
     }
 }
